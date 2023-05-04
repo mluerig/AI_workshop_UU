8ea182c1116d6cf815810e876052eab7b558ec99
mluerig <moritz.luerig@gmail.com>
2023-05-05 00:24:32 +0200

    update paths

--- scripts/01_phenopype_keras_model.py
+++ scripts/01_phenopype_keras_model.py
@@ -15,25 +15,22 @@ import cv2
 
 cv2.destroyAllWindows() 
 
-#%% pp project
+#%% ## whole body segmentation
 
-
-## whole body segmentation
-proj = pp.Project(r"C:\Users\mluerig\Desktop\cnn_demo")
-proj.add_model(r"D:\science\projects\2021_odonata_scans\models\keras\01_full_body.h5", model_id="fullbody_v1")
+proj = pp.Project(r"C:\Users\mluerig\Downloads\cnn_demo")
+proj.add_model(r"C:\Users\mluerig\Downloads\01_full_body.h5", model_id="fullbody_v1")
 
 ## run loop
 for path in proj.dir_paths:
     pp.Pype(path, tag="v1")
 
-## within body segmentation
-proj = pp.Project(r"C:\Users\mluerig\Desktop\cnn_demo")
-proj.add_model(r"D:\science\projects\2021_odonata_scans\models\keras\02_abdomen_v2.h5", model_id="abdomen_v2")
-proj.add_model(r"D:\science\projects\2021_odonata_scans\models\keras\02_thorax_v2.h5", model_id="thorax_v2")
-proj.add_model(r"D:\science\projects\2021_odonata_scans\models\keras\02_pronotum_v2.h5", model_id="pronotum_v2")
-proj.add_model(r"D:\science\projects\2021_odonata_scans\models\keras\02_head_v2.h5", model_id="head_v2")
+#%% within body segmentation
+
+proj = pp.Project(r"C:\Users\mluerig\Downloads\cnn_demo")
+proj.add_model(r"C:\Users\mluerig\Downloads\02_abdomen_v2.h5", model_id="abdomen_v2")
+proj.add_model(r"C:\Users\mluerig\Downloads\02_thorax_v2.h5", model_id="thorax_v2")
+proj.add_model(r"C:\Users\mluerig\Downloads\02_pronotum_v2.h5", model_id="pronotum_v2")
+proj.add_model(r"C:\Users\mluerig\Downloads\02_head_v2.h5", model_id="head_v2")
 
 for path in proj.dir_paths:
     pp.Pype(path, tag="v2")
-    
-    proj.export_zip(images=True)
